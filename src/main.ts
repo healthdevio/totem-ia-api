b82163ef81a5fb2de2320ef80cb48355765f56b2
pedrolauro15 <pedrolauro15@gmail.com>
2023-11-07 07:51:21 -0300

    added large limit base64 request

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(json({ limit: '100mb' }));
   app.enableCors({origin: '*'})
   await app.listen(3333)
   console.log(`App is running on 3333 🚀`);
